b395ed7fa9fd95cde0104f4fe09075ffcb7f9daf
Robertmonkey <rbrobertmonkey@gmail.com>
2025-07-29 21:20:52 -0400

    feat: begin fp patch

--- modules/PlayerController.js
+++ modules/PlayerController.js
@@ -1,5 +1,5 @@
 import * as THREE from '../vendor/three.module.js';
-import { getScene, getArena, getControllers } from './scene.js';
+import { getScene, getArena, getControllers, getCamera } from './scene.js';
 import { moveTowards } from './movement3d.js';
 import { spherePosToUv, uvToSpherePos } from './utils.js';
 import { state } from './state.js';
@@ -12,6 +12,7 @@ let avatar;
 let targetPoint = new THREE.Vector3();
 let raycaster;
 let laser;
+let crosshair;
 let rightController;
 let triggerDown = false;
 let gripDown = false;
@@ -25,17 +26,24 @@ export function initPlayerController() {
   const radius = arena.geometry.parameters.radius;
   avatar = new THREE.Mesh(
     new THREE.SphereGeometry(5, 16, 16),
-    new THREE.MeshBasicMaterial({ color: 0xffffff })
+    new THREE.MeshBasicMaterial({ color: 0x3498db })
   );
   avatar.name = 'playerAvatar';
   avatar.position.copy(uvToSpherePos(0.5, 0, radius));
   scene.add(avatar);
 
+  const chGeo = new THREE.RingGeometry(0.2, 0.25, 16);
+  const chMat = new THREE.MeshBasicMaterial({ color: 0x00ffff, side: THREE.DoubleSide });
+  crosshair = new THREE.Mesh(chGeo, chMat);
+  crosshair.visible = false;
+  crosshair.name = 'crosshair';
+  scene.add(crosshair);
+
   raycaster = new THREE.Raycaster();
   rightController = getControllers()[0];
 
   if (rightController) {
-    const material = new THREE.LineBasicMaterial({ color: 0xff0000 });
+    const material = new THREE.LineBasicMaterial({ color: 0x00ffff });
     const geometry = new THREE.BufferGeometry().setFromPoints([
       new THREE.Vector3(0, 0, 0),
       new THREE.Vector3(0, 0, -1)
@@ -115,6 +123,14 @@ export function updatePlayerController() {
     const uv = spherePosToUv(targetPoint, radius);
     window.mousePosition.x = uv.u * 2048;
     window.mousePosition.y = uv.v * 1024;
+    if (crosshair) {
+      crosshair.visible = true;
+      crosshair.position.copy(hit.point);
+      crosshair.lookAt(getCamera().position);
+    }
+  }
+  else if (crosshair) {
+    crosshair.visible = false;
   }
 
   moveTowards(avatar.position, targetPoint, state.player.speed, radius);
