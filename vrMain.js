8161e9652073bafa0fc56cda43491160c497378d
Robertmonkey <rbrobertmonkey@gmail.com>
2025-07-31 19:44:55 -0400

    FR-03: complete game over screen

--- vrMain.js
+++ vrMain.js
@@ -1,13 +1,14 @@
 import { initScene, getRenderer, getScene, getCamera } from './modules/scene.js';
 import { initPlayerController, updatePlayerController } from './modules/PlayerController.js';
 import { initUI, updateHud } from './modules/UIManager.js';
-import { initModals, startStage } from './modules/ModalManager.js';
+import { initModals, startStage, showModal } from './modules/ModalManager.js';
 import { initVrGameLoop, updateVrGameLoop } from './modules/vrGameLoop.js';
 import { initControllerMenu, updateControllerMenu } from './modules/ControllerMenu.js';
 import { AudioManager } from './modules/audio.js';
 import { loadPlayerState, state } from './modules/state.js';
 
 let initialized = false;
+let gameOverShown = false;
 
 export async function start(initialStage) {
   if (initialized) return;
@@ -28,10 +29,15 @@ export async function start(initialStage) {
       ? state.player.highestStageBeaten + 1
       : 1
   );
+  gameOverShown = false;
   initialized = true;
   getRenderer().setAnimationLoop(() => {
     updatePlayerController();
     updateVrGameLoop();
+    if (state.gameOver && !gameOverShown) {
+      showModal('gameOver');
+      gameOverShown = true;
+    }
     updateControllerMenu();
     updateHud();
     getRenderer().render(getScene(), getCamera());
