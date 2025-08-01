4f7563f2162c4d39074274b29874a1ef010d1dc9
Robertmonkey <rbrobertmonkey@gmail.com>
2025-08-01 00:34:08 -0400

    Update AssetManager.js

--- modules/AssetManager.js
+++ modules/AssetManager.js
@@ -1,5 +1,8 @@
 import * as THREE from '../vendor/three.module.js';
 
+// Lazy load loaders only when needed.
+let GLTFLoader;
+
 let instance = null;
 
 export class AssetManager {
@@ -9,9 +12,39 @@ export class AssetManager {
         }
         this.textureLoader = new THREE.TextureLoader();
         this.audioLoader = new THREE.AudioLoader();
+        this.gltfLoader = null;
         this.textureCache = new Map();
         this.audioCache = new Map();
+        this.modelCache = new Map();
         instance = this;
+    }
+
+    async _ensureGltfLoader() {
+        if (!this.gltfLoader) {
+            // This dynamic import will only run when loadModel is first called.
+            const loaderModule = await import('../vendor/addons/loaders/GLTFLoader.js');
+            this.gltfLoader = new loaderModule.GLTFLoader();
+        }
+    }
+
+    loadModel(url) {
+        if (this.modelCache.has(url)) {
+            return Promise.resolve(this.modelCache.get(url));
+        }
+        return new Promise(async (resolve, reject) => {
+            await this._ensureGltfLoader();
+            this.gltfLoader.load(url,
+                (gltf) => {
+                    this.modelCache.set(url, gltf);
+                    resolve(gltf);
+                },
+                undefined,
+                (error) => {
+                    console.error(`Failed to load model: ${url}`, error);
+                    reject(error);
+                }
+            );
+        });
     }
 
     loadTexture(url) {
