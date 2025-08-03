d01cf0d71f118c6a9bead8e977e805482f548ae7
Robertmonkey <rbrobertmonkey@gmail.com>
2025-08-02 22:40:00 -0400

    feat: add spherical pathfinding for enemies

--- modules/agents/JuggernautAI.js
+++ modules/agents/JuggernautAI.js
@@ -1,6 +1,6 @@
 import * as THREE from "../../vendor/three.module.js";
 import { BaseAgent } from '../BaseAgent.js';
-import { moveTowards } from '../movement3d.js';
+import { getSphericalDirection } from '../movement3d.js';
 import { state } from '../state.js';
 import { gameHelpers } from '../gameHelpers.js';
 
@@ -40,17 +40,25 @@ export class JuggernautAI extends BaseAgent {
             this.isCharging = false;
             this.lastChargeTime = now;
         } else {
-            // Move quickly towards the charge target
-            moveTowards(this.position, this.chargeTarget, 15 * speedMultiplier * delta, ARENA_RADIUS);
+            // Move quickly towards the charge target along the sphere
+            const dir = getSphericalDirection(this.position, this.chargeTarget);
+            const dist = this.position.distanceTo(this.chargeTarget);
+            this.position.add(dir.multiplyScalar(dist * 0.015 * 15 * speedMultiplier * delta));
+            this.position.normalize().multiplyScalar(ARENA_RADIUS);
+            this.lookAt(this.position.clone().add(dir));
             // If we reach the target, pick a new random one to simulate a bounce
             if (this.position.distanceTo(this.chargeTarget) < 2) {
                 this.chargeTarget.randomDirection().multiplyScalar(ARENA_RADIUS);
             }
         }
     } else {
-        // Normal movement
+        // Normal movement toward the player
         const playerPos = state.player.position;
-        moveTowards(this.position, playerPos, 0.8 * speedMultiplier * delta, ARENA_RADIUS);
+        const dir = getSphericalDirection(this.position, playerPos);
+        const dist = this.position.distanceTo(playerPos);
+        this.position.add(dir.multiplyScalar(dist * 0.015 * 0.8 * speedMultiplier * delta));
+        this.position.normalize().multiplyScalar(ARENA_RADIUS);
+        this.lookAt(this.position.clone().add(dir));
 
         // Check if it's time to charge
         if (now - this.lastChargeTime > 7000) {
